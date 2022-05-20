bfc6ee1e58fbd149351a47846261c519b38ee414
mahdi <falahati.mahdi@yahoo.com>
2022-05-20 19:48:44 +0430

    transaction list component add

---
+++ src/components/TransactionsList.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div>
+    Transferred {{item.amount}} {{item.amount > 0 ? 'to' : 'from'}} account {{item.account_id}} the current account balance
+    {{ item.amount }}
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "TransactionsList",
+   props:['item'],
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
