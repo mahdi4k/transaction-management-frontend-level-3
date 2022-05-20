6ffe5df5752ac88926342628727f3975e73d2616
mahdi <falahati.mahdi@yahoo.com>
2022-05-20 19:50:02 +0430

    simple unit test add

--- src/components/__tests__/HelloWorld.spec.js
+++ src/components/__tests__/HelloWorld.spec.js
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import TransactionsList from '../TransactionsList.vue'
 
-describe('HelloWorld', () => {
+describe('TransactionsList', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(TransactionsList, { props: { item: Object } })
+
   })
 })
